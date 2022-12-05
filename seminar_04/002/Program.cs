a8eafb4694a5c57548637c82233474dc6de4740e
Ivanov_Aleksandr <3911394@mail.ru>
2022-12-05 22:36:39 +0300

    Seminar 04 added

---
+++ seminar_04/002/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, 
+// значения которых лежат в отрезке [10,99]. 
+
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1); // [-9; 10)
+    }
+    return arr;
+}
+
+int FindElements(int[] arr, int leftRange, int rightRange)
+{
+    // leftRange <= X <= rightRange
+    int sum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= leftRange && arr[i] <= rightRange)
+            sum++;
+    }
+    return sum;
+}
+
+Console.Clear();
+
+int sizeArray = 123;
+int[] array = new int[sizeArray];
+array = FillArrayWithRandomNumbers(sizeArray, 0, 100);
+
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+int colElement = FindElements(array, 10, 99);
+Console.WriteLine("Найдено элементов: " + colElement);
+
---
+++ seminar_04/003/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+//  Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+//  Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+int[] Multiplayer(int[] oldArray, int newSizeArray)
+{
+    int[] newArray = new int[newSizeArray];
+
+    for (int i = 0; i < oldArray.Length / 2; i++)
+    {
+        newArray[i] = oldArray[i] * oldArray[oldArray.Length - 1 - i];
+    }
+
+    if (oldArray.Length % 2 != 0)
+    {
+        newArray[newArray.Length - 1] = oldArray[oldArray.Length / 2];
+    }
+    return newArray;
+}
+
+Console.Clear();
+
+int sizeArray = 7;
+
+int[] array = new int[sizeArray];
+array = FillArrayWithRandomNumbers(sizeArray, 0, 10);
+
+// int[] array = {1, 2, 3, 4, 5, 6, 7};
+
+Console.WriteLine("Исходный: [" + string.Join(", ", array) + "]");
+
+int newSizeArray = array.Length / 2 + array.Length % 2;
+int[] newArray = new int[newSizeArray];
+
+newArray = Multiplayer(array, newSizeArray);
+Console.WriteLine("Результат: [" + string.Join(", ", newArray) + "]");
